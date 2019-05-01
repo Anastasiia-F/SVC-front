231ead37863053cc2135589950b08829ab8e2ac3
Anastasiia <afesenk@gmail.com>
2019-05-01 17:20:11 +0300

    add 'thanks' page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
+import { ThanksComponent} from "./thanks/thanks.component";
 
 const routes: Routes = [
   {
@@ -14,7 +15,7 @@ const routes: Routes = [
   {
     path: 'payment-details',
     loadChildren: './payment-details/payment-details.module#PaymentDetailsModule'
-  },
+  },/*
   {
     path: 'auth',
     loadChildren: './auth/auth.module#AuthModule'
@@ -23,12 +24,11 @@ const routes: Routes = [
     path: 'main',
     canActivate: [AuthGuard],
     loadChildren: './dashboard/dashboard.module#DashboardModule'
-  },
-  // {
-  //   path: '',
-  //   redirectTo: '',
-  //   pathMatch: 'full'
-  // }
+  },*/
+  {
+    path: 'thank-you',
+    component: ThanksComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { carDataReducer } from './state/car-data.reducer';
 import { CarDataEffects } from './state/car-data.effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { ThanksComponent } from './thanks/thanks.component';
 
 const customNotifierConfig: NotifierOptions = {
   position: {
@@ -31,7 +33,8 @@ const customNotifierConfig: NotifierOptions = {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThanksComponent
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'svc'}),
@@ -46,6 +49,7 @@ const customNotifierConfig: NotifierOptions = {
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
     }),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
